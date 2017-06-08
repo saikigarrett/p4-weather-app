b25c31eb06b78ac506fd473923aa9fb455f0fd66
saikigarrett <garrettsaiki@hotmail.com>
2017-06-08 13:38:41 -0700

    Version 1
    Functionality completed
    Needs CSS
    Style app from here

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,25 @@
 // Scripts
 
-$.simpleWeather({
-    location: 'Spokane',
+// Toggle, Show/Hide Drawer
+$('.toggle-drawer').click( function() {
+  
+  $('aside').toggleClass('drawer');
+  
+});
+
+// Section 1 Weather
+$('#city-1').on('click', function() {
+  
+  $.simpleWeather({
+    location: 99004,
     unit: 'f',
     success: function(weather) {
       // Entire weather object
-      console.log(weather.code);
+      console.log(weather);
       
       // Display Data
-      $('.temp').text(weather.temp);
-      $('.city').text(weather.city);
-     
-      $('img').attr('src', weather.image);
-      
-      // Change Icon
-      $('i').attr('class', 'icon-' + weather.code )
-      
-      
-      if ( weather.code >= 26 && weather.code <= 30 ) {
-        
-        console.log('CLOUDY');
-        
-      }
-      
-      if ( weather.temp >= 80 ) {
-        
-        $('body').addClass('hot');
-        
-      }
-      
-      // Condition Code
-      // Reference Condition Codes
-      // https://developer.yahoo.com/weather/documentation.html#codes
-      console.log(weather.code);
+      $('#one .temp').text(weather.temp);
+      $('#one .city').text(weather.city);
       
     },
     error: function(error) {
@@ -40,6 +27,77 @@ $.simpleWeather({
       console.log('Look outside.');
     }
   
-  });
+  }); 
+  
+});
 
+
+// Section 2 Weather
+$('#city-2').on('click', function() {
+  
+  $.simpleWeather({
+    location: 98105,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('#two .temp').text(weather.temp);
+      $('#two .city').text(weather.city);
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look outside.');
+    }
+  
+  }); 
+  
+});
+
+
+
+// Section 3 Weather
+$('#city-3').on('click', function() {
+     
+    //load weather using your lat/long coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+      
+      // Check lat/long coordinates
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      
+      console.log(lat, long);
+      
+      // Send to SimpleWeather
+      getWeather( lat + ',' + long ); 
+    });
+   
+    var getWeather = function(location) {
+    
+   $.simpleWeather({
+    location: location,
+    unit: 'f',
+    success: function(weather) {
+      
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('#three .temp').text(weather.temp);
+      $('#three .city').text(weather.city);
+        
+
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Look Outside.');
+    }
+  
+  });
+    
+};
+  
+});
 
