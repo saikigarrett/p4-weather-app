8c3ad311743c5bb74a41663bf2f59d622f9b2911
garrettsaiki <garrettsaiki@eagles.ewu.edu>
2017-06-08 10:32:04 -0700

    Initial Commit
    Initial Commit

---
+++ js/plugin.js
@@ -0,0 +1,33 @@
+// jQuery Plugin
+
+$.simpleWeather({
+  location: 'Kirkland',
+  unit: 'f',
+  success: function(weather){
+    
+    //Show Weather
+     console.log(weather.city);
+     console.log(weather.region);
+    
+    console.log(weather.temp);
+    console.log(weather.humidity);
+    console.log(weather.currently);
+    
+    console.log(weather);
+    
+    // Display Weather
+    
+    $('.image img').attr( 'src', weather.image );
+    
+    $('.city').text(weather.city );
+        $('.state').text(weather.region );
+        $('.temp').text(weather.temp );
+        $('.humidity').text(weather.humidity );
+        $('.current').text(weather.currently );
+  },
+  
+  error: function(error){
+  console.log('Go look outside');
+  }
+  
+});
